0ddfa651dfd2a60818606cfa4fdc3e4ac970af99
Graham Gower <graham.gower@gmail.com>
2017-07-25 10:49:41 +0930

    Call MSMC on each population.
    GNU parallel has a bug with max command line parameters, preventing its use
    here.  Unfortunately, we must pass all contigs to MSMC on the command line
    and GNU parallel does not correctly determine the maximum allowed, so we
    are unable to proceed.

--- sim/msmc.sh
+++ sim/msmc.sh
@@ -3,22 +3,26 @@
 source ./model.settings
 
 # msmc needs a boatload of files
-ulimit -Sn 16384 || (echo "Cannot set 'ulimit -Sn 16384'"; exit 1)
+ulimit -Sn `ulimit -Hn` || (echo "Cannot set 'ulimit -Sn'"; exit 1)
 
-threads=4
+threads=1
 
-for m in `seq $models`; do
-	for s in `seq 0 $((samples-1))`; do
-		for x in $fraglevels; do
-			mkdir -p $odir/m$m/msmc1/s$s/x$x
-			$msmc \
-				-p $pparam \
-				--verbose \
-				-t $threads \
-				-i 30 \
-				-o $odir/m$m/msmc1/s$s/x$x/msmc1.out \
-				$odir/m$m/x$x/msmc.sample$s.*
+for p in `seq $pops`; do
+	for m in `seq $models`; do
+		for s in `seq 0 $((samples-1))`; do
+			for x in $fraglevels; do
+				mkdir -p $odir/m$m/msmc1/p$p/s$s/x$x
+				$msmc \
+					-p $pparam \
+					--verbose \
+					-t $threads \
+					-i 30 \
+					-o $odir/m$m/msmc1/p$p/s$s/x$x/msmc1.out \
+					$odir/m$m/p$p/x$x/msmc.sample$s.* \
+					|| exit 1
+			done
 		done
 	done &
+
 done
 wait
