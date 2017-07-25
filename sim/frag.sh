2008d555b755d5e10bf12108dfe70e564b4edbf3
Graham Gower <graham.gower@gmail.com>
2017-07-25 10:48:15 +0930

    Call fragmenter differently.

--- sim/frag.sh
+++ sim/frag.sh
@@ -2,15 +2,26 @@
 
 source ./model.settings
 
-for m in `seq $models`; do
-	for x in $fraglevels; do
-		mkdir -p $odir/m$m/x$x
-		./frag_ms2smc.pl \
-			-x $x \
-			-l $chrlen \
-			-p $odir/m$m/x$x/ \
-			< $odir/m$m/scrm.txt &
-	done
-done
-wait
+do_frag()
+{
+	odir=$1
+	chrlen=$2
+	m=$3
+	p=$4
+	x=$5
+	mkdir -p $odir/m$m/p$p/x$x
+	./frag_ms2smc.pl \
+		-x $x \
+		-l $chrlen \
+		-p $odir/m$m/p$p/x$x/ \
+		< $odir/m$m/p$p/scrm.txt
+}
+export -f do_frag
 
+for p in `seq $pops`; do
+	for m in `seq $models`; do
+		for x in $fraglevels; do
+			echo do_frag $odir $chrlen $m $p $x
+		done
+	done
+done | parallel -j 8
