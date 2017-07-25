27ef0958f9b43c4a47a5b569aab9fedfd2ddbce8
Graham Gower <graham.gower@gmail.com>
2017-07-25 10:49:09 +0930

    Call psmc for each population.

--- sim/psmc.sh
+++ sim/psmc.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 source ./model.settings
-#odir=out-truncpsmc
 
-for m in `seq $models`; do
-	for s in `seq 0 $((samples-1))`; do
-		for x in $fraglevels; do
-			mkdir -p $odir/m$m/psmc/s$s/x$x
-			$psmc \
-				-p $pparam \
-				-o $odir/m$m/psmc/s$s/x$x/psmc.out \
-				$odir/m$m/x$x/psmc.sample$s
+for p in `seq $pops`; do
+	for m in `seq $models`; do
+		for s in `seq 0 $((samples-1))`; do
+			for x in $fraglevels; do
+				mkdir -p $odir/m$m/psmc/p$p/s$s/x$x
+				mkdir -p $odir/m$m/psmc-trunc/p$p/s$s/x$x
+				echo $psmc \
+					-p $pparam \
+					-o $odir/m$m/psmc/p$p/s$s/x$x/psmc.out \
+					$odir/m$m/p$p/x$x/psmc.sample$s
+				echo $psmc \
+					-p $pparam \
+					-o $odir/m$m/psmc-trunc/p$p/s$s/x$x/psmc.out \
+					$odir/m$m/p$p/x$x/psmc-trunc.sample$s
 			done
-	done &
-done
-wait
+		done
+	done
+done | parallel -j 8
