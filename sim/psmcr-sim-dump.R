40857968fc809b4adf1f3f41f4b3609bd1842d1a
Graham Gower <graham.gower@gmail.com>
2017-07-26 11:06:33 +0930

    Add population column to the data.frame.

--- sim/psmcr-sim-dump.R
+++ sim/psmcr-sim-dump.R
@@ -8,12 +8,14 @@ g <- 1
 
 models <- 1:3
 fragsizes <- c("100000000", "10000000", "1000000", "100000", "10000")
-nsamples <- 100
+nsamples <- 10
+npops <- 10
 
 d = data.frame(prog=factor(levels=c("PSMC", "PSMC-trunc","MSMC1")),
 		model=factor(levels=models),
 		fragsize=vector("numeric"),
 		sample=vector("numeric"),
+		population=vector("numeric"),
 		rd=vector("numeric"), # EM round
 		GOF=vector("numeric"),
 		ll=vector("numeric"),
@@ -23,26 +25,28 @@ d = data.frame(prog=factor(levels=c("PSMC", "PSMC-trunc","MSMC1")),
 		k=vector("numeric"), # time bin
 		t=vector("numeric"), # time in 2*N0 generations
 		lambda=vector("numeric"),
-		pi <- vector("numeric"), # PSMC only, actually \pi_k*C_pi
-		sigma <- vector("numeric"),
-		post_sigma <- vector("numeric"),
-		time <- vector("numeric"), # time in years
-		N <- vector("numeric")) # effective pop. size
+		pi=vector("numeric"), # PSMC only, actually \pi_k*C_pi
+		sigma=vector("numeric"),
+		post_sigma=vector("numeric"),
+		time=vector("numeric"), # time in years
+		N=vector("numeric")) # effective pop. size
 
 for (m in models) {
 	for (x in fragsizes) {
-		for (s in 1:nsamples) {
-			psmc_file <- sprintf("out/m%d/psmc/s%d/x%s/psmc.out", m, s-1, x)
-			psmc <- lastround(parse_psmc(psmc_file, mu, g))
-			d = rbind(d, cbind(prog="PSMC", model=m, fragsize=as.integer(x), sample=s, psmc))
+		for (p in 1:npops) {
+			for (s in 1:nsamples) {
+				psmc_file <- sprintf("out/m%d/psmc/p%d/s%d/x%s/psmc.out", m, p, s-1, x)
+				psmc <- lastround(parse_psmc(psmc_file, mu, g))
+				d = rbind(d, cbind(prog="PSMC", model=m, fragsize=as.integer(x), sample=s, population=p, psmc))
 
-			psmc_file <- sprintf("out-truncpsmc/m%d/psmc/s%d/x%s/psmc.out", m, s-1, x)
-			psmc <- lastround(parse_psmc(psmc_file, mu, g))
-			d = rbind(d, cbind(prog="PSMC-trunc", model=m, fragsize=as.integer(x), sample=s, psmc))
+				psmc_file <- sprintf("out/m%d/psmc-trunc/p%d/s%d/x%s/psmc.out", m, p, s-1, x)
+				psmc <- lastround(parse_psmc(psmc_file, mu, g))
+				d = rbind(d, cbind(prog="PSMC-trunc", model=m, fragsize=as.integer(x), sample=s, population=p, psmc))
 
-			msmc1_filebase <- sprintf("out/m%d/msmc1/s%d/x%s/msmc1.out", m, s-1, x)
-			msmc1 = lastround(parse_msmc(msmc1_filebase, mu, g))
-			d = rbind(d, cbind(prog="MSMC1", model=m, fragsize=as.integer(x), sample=s, msmc1))
+				msmc1_filebase <- sprintf("out/m%d/msmc1/p%d/s%d/x%s/msmc1.out", m, p, s-1, x)
+				msmc1 = lastround(parse_msmc(msmc1_filebase, mu, g))
+				d = rbind(d, cbind(prog="MSMC1", model=m, fragsize=as.integer(x), sample=s, population=p, msmc1))
+			}
 		}
 	}
 }
